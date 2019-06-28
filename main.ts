5feb019bdbe4a1ab2083b1923d78012b37c2924c
henrywycheng <51484937+henrywycheng@users.noreply.github.com>
2019-06-28 22:39:53 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -16,14 +16,20 @@ namespace TCS34725RGB {
 		return false
 	}
 	basic.pause(200)
+	/* G54 TCS34725 RGBC color sensor Enable write addr 0x29 register 0x00 command 0x80 */
+	/* write 0x8003 */
 	pins.i2cWriteNumber(41,32771,NumberFormat.UInt16BE,false)
 	basic.pause(200)
+	/* G54 TCS34725 RGBC color sensor Enable read addr 0x29 register 0x00 command 0x80 */
+	/* check for 0x03 */
 	pins.i2cWriteNumber(41,128,NumberFormat.UInt8LE,false)
 	basic.pause(200)
 	if (pins.i2cReadNumber(41, NumberFormat.UInt8LE, false) !=3) {
 		return false
 	}
 	basic.pause(200)
+	/* G54 TCS34725 RGBC color sensor Status addr 0x29 register 0x13 command 0x93 */
+	/* check for 0x11 */
 	pins.i2cWriteNumber(41,147,NumberFormat.UInt8LE,false)
 	basic.pause(200)
 	if (pins.i2cReadNumber(41, NumberFormat.UInt8LE, false) !=17) {
